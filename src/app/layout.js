7f6f353c67065e98626e37013497a2fbc2f27c9a
Hasan Sargin <hasan.sargin@hotmail.com>
2026-04-08 01:21:30 +0300

    google ads tag eklendi

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,20 @@
-export const metadata = {
-  title: "Prof Sigorta | Doğru Teklif, Doğru Fiyat",
-  description:
-    "Trafik, kasko, DASK, sağlık ve diğer sigortalar için hızlı teklif alın.",
-};
-
 export default function RootLayout({ children }) {
   return (
     <html lang="tr">
+      <head>
+        {/* Google Ads Tag */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-18071924771"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-18071924771');
+            `,
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   );
